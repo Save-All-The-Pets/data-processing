8b12eac87f33707b827a60384ccc91b803c0abbc
alex-antonison <30709680+alex-antonison@users.noreply.github.com>
2021-01-04 20:59:55 -0600

    Add sample file access (#3)
    * Removes photos from processing step
    
    * Add access data example
    
    Co-authored-by: Alexander Antonison <adantonison@gmail.com>

--- processing-mssql-backup/process_mssql_bak.sh
+++ processing-mssql-backup/process_mssql_bak.sh
@@ -9,10 +9,6 @@ docker run --name mssql_server -d -p 1433:1433 -e sa_password=Local_password1 -e
 # loads the SaveAllThePets.bak file
 sqlcmd -S localhost,1433 -i import_database_file.sql -d master -U sa -P Local_password1!
 
-sqlcmd -S localhost,1433 -i export_photos.sql -d master -U sa -P Local_password1! -o data/Photos_tmp.csv -s "|" -W
-sed '2d' data/Photos_tmp.csv > data/Photos.csv
-rm data/Photos_tmp.csv
-
 sqlcmd -S localhost,1433 -i export_dataexport.sql -d master -U sa -P Local_password1! -o data/DataExport_tmp.csv -s "|" -W
 sed '2d' data/DataExport_tmp.csv > data/DataExport.csv
 rm data/DataExport_tmp.csv
