a023654ef75595acbe74c5288f64936ac1a643fc
Alexander Antonison <adantonison@gmail.com>
2021-01-12 20:32:02 -0600

    Add permission for running dbt

--- database-setup/database-setup.sql
+++ database-setup/database-setup.sql
@@ -19,4 +19,6 @@ alter group data_users add user ned;
 grant usage on schema shelter_data to data_users;
 grant all on all tables in schema shelter_data to data_users;
 
+grant create on database save_all_the_pets to group data_users;
+
 CREATE EXTENSION aws_s3 CASCADE;
