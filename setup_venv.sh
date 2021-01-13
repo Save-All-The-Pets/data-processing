bfee0b16001734682761aea11a9e9f7a4de1d43f
Alexander Antonison <adantonison@gmail.com>
2021-01-12 20:31:29 -0600

    Change python to 3.8.5

--- setup_venv.sh
+++ setup_venv.sh
@@ -21,12 +21,12 @@ function python_install() {
 # requirements.txt was set up with python 3.9
 # and it is recommended that it is used when setting
 # up environment
-if [[ python_major -eq 3 && python_minor -eq 9 ]] 
+if [[ python_major -eq 3 && python_minor -eq 8 ]] 
 then
-    echo "On Python 3.9 and good to install"
+    echo "On Python 3.8 and good to install"
     python_install
 else
-    read -p "Expecting Python 3.9 - do you want to proceed with setup? (Y/n) " -n 1 -r
+    read -p "Expecting Python 3.8 - do you want to proceed with setup? (Y/n) " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
