7e29f041d97df50ed27dbc6faf201415cdccc928
Alexander Antonison <adantonison@gmail.com>
2021-01-12 19:49:07 -0600

    change to csv

--- example/access_data_example.py
+++ example/access_data_example.py
@@ -1,2 +1,3 @@
 import pandas as pd
-dataexport_df = pd.read_csv("s3://save-all-the-pets/data/DataExport.csv", sep ="|")
+dataexport_df = pd.read_csv("s3://save-all-the-pets/DataExport_for_postgres.csv")
+dataexport_df.head()
