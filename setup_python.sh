0e9804092efa1feda2235c3bcd705fd7775ccfdb
alex-antonison <30709680+alex-antonison@users.noreply.github.com>
2021-01-15 16:55:57 -0600

    Update setup_python.sh

--- setup_python.sh
+++ setup_python.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
 
 # ## Install pyenv
-PYTHON_VERSION="3.9.0"
+PYTHON_VERSION="3.8.5"
 
 pyenv install $PYTHON_VERSION
 pyenv global $PYTHON_VERSION
