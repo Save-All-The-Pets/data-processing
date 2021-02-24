727a9bad88ece4fae773a977263bbd18669bd1a5
Alexander Antonison <adantonison@gmail.com>
2021-02-24 03:27:15 -0600

    Change default

--- save_all_the_pets/models/shelter_data/shelter_photos.sql
+++ save_all_the_pets/models/shelter_data/shelter_photos.sql
@@ -8,9 +8,10 @@ WITH base AS (
     SELECT
         {{ dbt_utils.surrogate_key(['photo_1']) }} :: VARCHAR(32) AS photo_id,
         photo_1 AS photo_url,
-        NULL AS inferred_fur_type,
-        NULL AS inferred_color,
-        NULL AS inferred_breed
+        NULL AS url_type,
+        '{}' :: jsonb AS inferred_fur_type,
+        '{}' :: jsonb AS inferred_color,
+        '{}' :: jsonb AS inferred_breed
     FROM
         {{ ref(
             'shelter_data'
@@ -21,9 +22,10 @@ WITH base AS (
     SELECT
         {{ dbt_utils.surrogate_key(['photo_2']) }} :: VARCHAR(32) AS photo_id,
         photo_2 AS photo_url,
-        NULL AS inferred_fur_type,
-        NULL AS inferred_color,
-        NULL AS inferred_breed
+        NULL AS url_type,
+        '{}' :: jsonb AS inferred_fur_type,
+        '{}' :: jsonb AS inferred_color,
+        '{}' :: jsonb AS inferred_breed
     FROM
         {{ ref(
             'shelter_data'
@@ -34,9 +36,10 @@ WITH base AS (
     SELECT
         {{ dbt_utils.surrogate_key(['photo_3']) }} :: VARCHAR(32) AS photo_id,
         photo_3 AS photo_url,
-        NULL AS inferred_fur_type,
-        NULL AS inferred_color,
-        NULL AS inferred_breed
+        NULL AS url_type,
+        '{}' :: jsonb AS inferred_fur_type,
+        '{}' :: jsonb AS inferred_color,
+        '{}' :: jsonb AS inferred_breed
     FROM
         {{ ref(
             'shelter_data'
